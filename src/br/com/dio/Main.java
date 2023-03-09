0269023b6fe4d40d528f4b2c8756768b6707f527
Erik-Vasconcelos <erikvasconcelos2018@gmail.com>
2023-03-09 14:05:43 -0300

    alteracoes

--- src/br/com/dio/Main.java
+++ src/br/com/dio/Main.java
@@ -1,5 +1,8 @@
 package br.com.dio;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         int a = 5;
@@ -7,6 +10,13 @@ public class Main {
 
         System.out.println("Hello world!");
 
+        Gato gato1 = new Gato();
+
+        List lista = new ArrayList<>();
         Gato gato = new Gato();
     }
+}
+
+class Livro{
+
 }
